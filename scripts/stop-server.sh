4dfe60a0cea41da272f2e5a167e30ab56613b4ff
akeshpatil@gmail.com <akeshpatil@gmail.com>
2015-10-23 12:56:31 +0200

    Updated server directory

--- scripts/stop-server.sh
+++ scripts/stop-server.sh
@@ -1,3 +1,3 @@
 service tomcat7 stop
 rm -r /usr/share/tomcat7/webapps/*
-cp -R /var/lib/jenkins/workspace/CodeDeployDemo/target/* /usr/local/tomcat/webapps/
+cp -R /var/lib/jenkins/workspace/CodeDeployDemo/target/* /usr/share/tomcat7/webapps
