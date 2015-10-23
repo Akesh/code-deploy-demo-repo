69be4d10cd8d125093da9ae367018544909b691f
akeshpatil@gmail.com <akeshpatil@gmail.com>
2015-10-23 12:35:23 +0200

    Added tomcat setup

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,2 +1,1 @@
-#docker build -t web-app-demo /Dockerfile
-#docker run -d -p 80:8080 web-app-demo
+service tomcat7 start
--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,3 +1,2 @@
-docker pull java:8-jdk
-docker pull tomcat
-mkdir -p mywebapp	
+yum install tomcat7 tomcat7-webapps
+mkdir -p mywebapp
